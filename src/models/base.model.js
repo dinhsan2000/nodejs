e29d2a11cc2c9fa89ee9b7301ded3dedae6f1ca2
Đinh San <dinhsan200@gmail.com>
2024-05-11 02:36:12 +0700

    add document for method

--- src/models/base.model.js
+++ src/models/base.model.js
@@ -16,6 +16,14 @@ export class BaseModel extends Database {
     }
   }
 
+  /**
+   * Get all data from the table or with a condition if set
+   *
+   * @returns {Promise<Array>} Data from the table
+   * @throws {Error} If there's an error fetching data
+   * @finally Close the connection
+   * 
+   */
   async get() {
     if (!this.sql || this.sql === '') {
       this.sql = `SELECT * FROM ${this.table}`;
@@ -33,6 +41,15 @@ export class BaseModel extends Database {
     }
   }
 
+  /**
+   * Find data by ID in the table
+   *
+   * @param {number} id ID of the data
+   * @returns {Promise<Object>} Data from the table
+   * @throws {Error} If there's an error fetching data
+   * @finally Close the connection
+   * 
+   */
   async find(id) {
     try {
       const [row] = await this.pool.query(
@@ -46,6 +63,15 @@ export class BaseModel extends Database {
     } // Don't close the connection here
   }
 
+  /**
+   * Find data by a condition in the table
+   *
+   * @param {Object} condition Condition to find data
+   * @returns {Promise<Array>} Data from the table
+   * @throws {Error} If there's an error fetching data
+   * @finally Close the connection
+   * 
+   */
   where(condition) {
     let sql = `SELECT * FROM ${this.table} WHERE `;
     const keys = Object.keys(condition);
@@ -61,16 +87,43 @@ export class BaseModel extends Database {
     return this;
   }
 
+  /**
+   * Find data where a field is not null in the table 
+   *
+   * @param {string} field Field to check if it's not null
+   * @returns {Promise<Array>} Data from the table
+   * @throws {Error} If there's an error fetching data
+   * @finally Close the connection
+   * 
+   */
   whereNotNull(field) {
     this.sql = `SELECT * FROM ${this.table} WHERE ${field} IS NOT NULL`;
     return this;
   }
 
+  /**
+   * Select fields to fetch from the table 
+   * 
+   * @param  {...string} fields Fields to fetch
+   * @returns {Promise<Array>} Data from the table
+   * @throws {Error} If there's an error fetching data
+   * @finally Close the connection
+   * 
+   */
   async select(...fields) {
     this.sql = `SELECT ${fields.join(', ')} FROM ${this.table}`;
     return this;
   }
 
+  /**
+   * Create data in the table 
+   * 
+   * @param {Object} data Data to create
+   * @returns {Promise<Object>} Created data
+   * @throws {Error} If there's an error creating data
+   * @finally Close the connection
+   * 
+   */
   async create(data) {
     try {
       const [rows] = await this.pool.query(`INSERT INTO ${this.table} SET ?`, data);
@@ -83,6 +136,16 @@ export class BaseModel extends Database {
     }
   }
 
+  /**
+   * Update data in the table
+   * 
+   * @param {number} id ID of the data
+   * @param {Object} data Data to update
+   * @returns {Promise<Object>} Updated data
+   * @throws {Error} If there's an error updating data
+   * @finally Close the connection
+   * 
+   */
   async update(id, data) {
     try {
       await this.pool.query(`UPDATE ${this.table} SET ? WHERE id = ?`, [data, id]);
@@ -95,6 +158,15 @@ export class BaseModel extends Database {
     }
   }
 
+  /**
+   * Delete data from the table
+   * 
+   * @param {number} id ID of the data
+   * @returns {Promise<void>}
+   * @throws {Error} If there's an error deleting data
+   * @finally Close the connection
+   * 
+   */
   async delete(id) {
     try {
       await this.pool.query(`DELETE FROM ${this.table} WHERE id = ?`, [id]);
@@ -106,6 +178,13 @@ export class BaseModel extends Database {
     }
   }
 
+  /**
+   * Delete hidden fields from the data
+   * 
+   * @param {Array} data Data to delete hidden fields
+   * @returns {Array} Data without hidden fields
+   * 
+   */
   async deleteHiddenFields(data) {
     if (this.hidden.length === 0) return data;
 
